7913c7d03eb5bb62cf91419e4ecf9545a3639904
zeroyip175 <zeroyip175@gmail.com>
2023-11-24 22:46:33 +0800

    vfor vmode done

--- js/tut.js
+++ js/tut.js
@@ -2,10 +2,12 @@ var ninja = Vue.createApp ({
     data(){
         return{
             title: 'Welcome to ninja world',
+            ninjaelement: "Ability: Fire ball jutsu ☄️",
             ninjadatas: [
                 {name: "Name: John Smith"},
                 {health: "Health: 100/100"},
                 {level: "lv: 20"}
+            
             ]
         }
     }
