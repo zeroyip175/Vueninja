77046b67307c85eaf2d466a49800c01e32a59227
cashcow <cashcowtech2021@gmail.com>
2023-12-01 17:57:33 +0800

    method add capitalizedfirstletter

--- js/tut.js
+++ js/tut.js
@@ -7,17 +7,18 @@ var ninja = Vue.createApp ({
                 {members:"Ninjas Members", health:"Health", level:"Level"}
             ],
             ninjadatas: [
-                {name:"John Smith", health: "30/100", level:10},
-                {name:"Naruto Uzumaki", health:"40/100", level:1},
-                {name:"Sasuke Uchiha", health: "100/100", level:80},
-                {name:"Shikamaru Nara",health:"98/100",level:76},
-                {name:"Hinata Hyuga",health:"98/100",level:73},
-                {name:"Jiraiya",health:"98/100",level:72},
-                {name:"Gaara",health:"98/100",level:71},
-                {name:"Rock Lee",health:"98/100",level:75},
-                {name:"Itachi Uchiha",health:"98/100",level:100}
+                {name:"john Smith", health: "30/100", level:10},
+                {name:"naruto Uzumaki", health:"40/100", level:1},
+                {name:"sasuke Uchiha", health: "100/100", level:80},
+                {name:"shikamaru Nara",health:"98/100",level:76},
+                {name:"hinata Hyuga",health:"98/100",level:73},
+                {name:"jiraiya",health:"98/100",level:72},
+                {name:"gaara",health:"98/100",level:71},
+                {name:"rock Lee",health:"98/100",level:75},
+                {name:"itachi Uchiha",health:"98/100",level:100}
             ],
             newninjastudent : " ",
+
         }
     },
     methods: {
@@ -30,8 +31,14 @@ var ninja = Vue.createApp ({
 
         deleteNinjaStudent(index){
             this.ninjadatas.splice(index,1);
+        },
+        capitalizedfirstletter(value){
+            if (!value) return '';
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
         }
     },
+    
 })
 
 ninja.mount('#gameapp')
