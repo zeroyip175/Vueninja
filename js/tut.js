930fa3b6a7d48afac469a7de2b39e9c651fce87f
cashcow <cashcowtech2021@gmail.com>
2023-12-05 15:30:15 +0800

    sorted function

--- js/tut.js
+++ js/tut.js
@@ -4,21 +4,26 @@ var ninja = Vue.createApp ({
             title: 'Welcome to ninja world',
             ninjaelement: "Ability: Fire ball jutsu ☄️",
             tabletitles:[
-                {members:"Ninjas Members", health:"Health", level:"Level"}
+                {members:"Ninjas Members", health:"Health", level:"Level", dob: "Date of Birth"}
             ],
             ninjadatas: [
-                {name:"john Smith", health: "30/100", level:10},
-                {name:"naruto Uzumaki", health:"40/100", level:1},
-                {name:"sasuke Uchiha", health: "100/100", level:80},
-                {name:"shikamaru Nara",health:"98/100",level:76},
-                {name:"hinata Hyuga",health:"98/100",level:73},
-                {name:"jiraiya",health:"98/100",level:72},
-                {name:"gaara",health:"98/100",level:71},
-                {name:"rock Lee",health:"98/100",level:75},
-                {name:"itachi Uchiha",health:"98/100",level:100}
+                {name:"john Smith", health: "30/100", level:10, dob:"1990-10-18"},
+                {name:"naruto Uzumaki", health:"40/100", level:1, dob:"1991-02-03"},
+                {name:"sasuke Uchiha", health: "100/100", level:80, dob:"1994-05-13"},
+                {name:"shikamaru Nara",health:"98/100",level:76, dob:"1992-05-13"},
+                {name:"hinata Hyuga",health:"98/100",level:73, dob:"1998-02-13"},
+                {name:"jiraiya",health:"98/100",level:72, dob:"1993-10-18"},
+                {name:"gaara",health:"98/100",level:71, dob:"1994-10-18"},
+                {name:"rock Lee",health:"98/100",level:75, dob:"1995-10-18"},
+                {name:"itachi Uchiha",health:"98/100",level:100, dob:"1997-10-18"}
             ],
             newninjastudent : " ",
 
+        }
+    },
+    computed:{
+        sortedNinjaStudents: function(){
+            return this.ninjadatas.reverse();
         }
     },
     methods: {
@@ -36,8 +41,12 @@ var ninja = Vue.createApp ({
             if (!value) return '';
             value = value.toString();
             return value.charAt(0).toUpperCase() + value.slice(1);
-        }
+        },
+        ageFromDOB: function(dob){
+            return moment().diff(dob,"years");
+        } //if you need to use function like momnet -> add <script>index page</script>
     },
+    
     
 })
 
